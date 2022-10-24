72f3bfd3571934a4970dbf525fdd16f7e1527a04
flo <inbox.733@gmail.com>
2022-10-24 10:45:36 -0600

    Cancel button location should be consistent - i.e right of other buttons to match other views.

--- src/gui/element/wallet/operation/open.rs
+++ src/gui/element/wallet/operation/open.rs
@@ -257,9 +257,9 @@ pub fn data_container<'a>(
 
     let cancel_button: Element<Interaction> = cancel_button.into();
     let button_row = Row::new()
-        .push(cancel_button.map(Message::Interaction))
+        .push(submit_button.map(Message::Interaction))
         .push(Space::new(Length::Units(unit_spacing), Length::Units(0)))
-        .push(submit_button.map(Message::Interaction));
+        .push(cancel_button.map(Message::Interaction));
 
     let column = Column::new()
         .push(title_row)
