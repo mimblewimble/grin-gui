cf353de029f803d5fb2fef09911cd083e154c5c1
Yeastplume <yeastplume@gmail.com>
2022-11-03 16:58:17 +0000

    add wallet view address, amounts and begin to fill out transaction flow

--- src/gui/element/wallet/operation/apply_tx.rs
+++ src/gui/element/wallet/operation/apply_tx.rs
@@ -14,7 +14,7 @@ use std::path::PathBuf;
 use super::tx_list::{HeaderState, TxList};
 
 use {
-    super::super::super::{DEFAULT_FONT_SIZE, DEFAULT_HEADER_FONT_SIZE, DEFAULT_PADDING},
+    super::super::super::{DEFAULT_FONT_SIZE, DEFAULT_HEADER_FONT_SIZE, SMALLER_FONT_SIZE, DEFAULT_PADDING},
     crate::gui::{style, GrinGui, Interaction, Message},
     crate::localization::localized_string,
     crate::Result,
@@ -31,23 +31,29 @@ use {
 
 pub struct StateContainer {
     pub back_button_state: button::State,
+    pub copy_address_button_state: button::State,
+    pub address_state: text_input::State,
+    pub address_value: String,
 }
 
 impl Default for StateContainer {
     fn default() -> Self {
         Self {
             back_button_state: Default::default(),
+            copy_address_button_state: Default::default(),
+            address_state: Default::default(),
+            address_value: Default::default(),
         }
     }
 }
 
 #[derive(Debug, Clone, PartialEq, Eq, Hash, Serialize, Deserialize)]
-pub enum Action {
-}
+pub enum Action {}
 
 #[derive(Debug, Clone)]
 pub enum LocalViewInteraction {
     Back,
+    Address(String),
 }
 
 pub fn handle_message<'a>(
@@ -55,16 +61,18 @@ pub fn handle_message<'a>(
     message: LocalViewInteraction,
 ) -> Result<Command<Message>> {
     /*let state = &mut grin_gui
-        .wallet_state
-        .operation_state
-        .home_state
-        .action_menu_state;*/
+    .wallet_state
+    .operation_state
+    .home_state
+    .action_menu_state;*/
     match message {
         LocalViewInteraction::Back => {
-            log::debug!(
-                "Interaction::WalletOperationApplyTxViewInteraction(Back)"
-            );
-            grin_gui.wallet_state.operation_state.mode = crate::gui::element::wallet::operation::Mode::Home;
+            log::debug!("Interaction::WalletOperationApplyTxViewInteraction(Back)");
+            grin_gui.wallet_state.operation_state.mode =
+                crate::gui::element::wallet::operation::Mode::Home;
+        }
+        LocalViewInteraction::Address(_) => {
+
         }
     }
     Ok(Command::none())
@@ -105,10 +113,58 @@ pub fn data_container<'a>(
         .padding(6)
         .spacing(20);
 
+    let address_name = Text::new(localized_string("slatepack-address-name"))
+        .size(DEFAULT_FONT_SIZE)
+        .horizontal_alignment(alignment::Horizontal::Left);
+
+    let address_name_container =
+        Container::new(address_name).style(style::NormalBackgroundContainer(color_palette));
+
+    let address_input = TextInput::new(&mut state.address_state, "", &state.address_value, |s| {
+        Interaction::WalletOperationApplyTxViewInteraction(LocalViewInteraction::Address(s))
+    })
+    .size(DEFAULT_FONT_SIZE)
+    .padding(6)
+    .width(Length::Units(400))
+    .style(style::AddonsQueryInput(color_palette));
+
+    let address_input: Element<Interaction> = address_input.into();
+
+    let copy_address_button = Button::new(
+        &mut state.copy_address_button_state,
+        Text::new(localized_string("copy-to-clipboard"))
+            .size(SMALLER_FONT_SIZE)
+            .horizontal_alignment(alignment::Horizontal::Center),
+    )
+    .style(style::NormalTextButton(color_palette))
+    .on_press(Interaction::WriteToClipboard(
+        state.address_value.clone(),
+    ));
+
+    let copy_address_button: Element<Interaction> = copy_address_button.into();
+
+    let address_row = Row::new()
+    .push(address_input)
+    .push(copy_address_button)
+    .spacing(DEFAULT_PADDING);
+
+    let address_row: Element<Interaction> = address_row.into();
+
+    let address_instruction_container = Text::new(localized_string("address-instruction"))
+        .size(SMALLER_FONT_SIZE)
+        .horizontal_alignment(alignment::Horizontal::Left);
+
+    let address_instruction_container =
+        Container::new(address_instruction_container).style(style::NormalBackgroundContainer(color_palette));
+
+
     let column = Column::new()
         .push(title_row)
-        .padding(10) 
-        .align_items(Alignment::Center);
+        .push(address_name_container)
+        .push(address_instruction_container)
+        .push(address_row.map(Message::Interaction))
+        .spacing(DEFAULT_PADDING)
+        .align_items(Alignment::Start);
 
     Container::new(column)
         .center_y()
