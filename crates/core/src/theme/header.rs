590326456a17881c239b73140b4b4b14ad499f50
flo <inbox.733@gmail.com>
2022-11-27 11:14:58 -0700

    Default header style.

--- crates/core/src/theme/header.rs
+++ crates/core/src/theme/header.rs
@@ -1,6 +1,6 @@
-use grin_gui_widgets::style::header::{StyleSheet, Appearance};
-use iced::{Background, Color};
 use super::Theme;
+use grin_gui_widgets::style::header::{Appearance, StyleSheet};
+use iced::{Background, Color};
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Default)]
 pub enum HeaderStyle {
@@ -14,28 +14,22 @@ impl StyleSheet for Theme {
     fn appearance(&self, style: &Self::Style) -> Appearance {
         match style {
             HeaderStyle::Default => Appearance {
-                text_color: Some(self.palette.normal.primary),
-                background: Some(Background::Color(self.palette.base.foreground)),
+                text_color: None,
+                background: None,
                 border_radius: 0.0,
                 border_width: 0.0,
                 border_color: Color::TRANSPARENT,
-                offset_left: 10.0,
-                offset_right: 25.0,
+                offset_right: 0.0,
+                offset_left: 0.0,
             },
         }
     }
 
     fn hovered(&self, style: &Self::Style) -> Appearance {
         let appearance = self.appearance(style);
-
-        match style {
-            HeaderStyle::Default => Appearance {
-                background: Some(Background::Color(Color {
-                    a: 0.60,
-                    ..self.palette.normal.primary
-                })),
-                ..appearance
-            },
+        Appearance {
+            background: None,
+            ..appearance
         }
     }
 }
